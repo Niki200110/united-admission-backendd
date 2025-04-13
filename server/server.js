e13bc0984224d1402060ed03050c4aa27aaab694
Niki200110 <fanrohit65@gmail.com>
2025-04-13 12:48:29 +0530

    fix: added Netlify domain to CORS

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://charming-malasada-219d82.netlify.app'],
   credentials: true
 }));
 app.use(express.json());
